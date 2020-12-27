b4bc030a1d804e7ea2694c6dce3668795bbfd3a5
Yannic Kitten <yannic.kitten@outlook.de>
2020-12-27 17:25:42 +0100

    Fixed functionality issue

--- install.sh
+++ install.sh
@@ -3,11 +3,13 @@
 REPO_LOCATION=${0%/*}
 
 if [[ -d $HOME/.config/nvim ]] ; then
-    echo -e "Directory $HOME/.config/nvim already exists. Do You want to replace it?\nAll your Data in this directory will be deleted!"
+    echo -e "Directory or file $HOME/.config/nvim already exists. Do You want to replace it?\nAll your Data in this directory will be deleted!"
     read -p "Are you sure to replace this directory? [y/N] " REPLACE_NVIM
-fi
-
-if [[ "$REPLACE_NVIM" == [yY]* ]] ; then
-    rm -rf $HOME/.config/nvim
+    if [[ "$REPLACE_NVIM" == [yY]* ]] ; then
+        rm -rf $HOME/.config/nvim
+        ln -s $REPO_LOCATION/nvim $HOME/.config/nvim
+    fi
+else
     ln -s $REPO_LOCATION/nvim $HOME/.config/nvim
 fi
+
